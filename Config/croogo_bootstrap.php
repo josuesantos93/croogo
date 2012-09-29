68e72b54327dbeccc2a8d86a65287a90251551fd
Josué Silva Santos <josue.silva.santos@gmail.com>
2012-09-29 17:06:16 -0300

    Update Config/croogo_bootstrap.php
    conflict with file and directory.

--- Config/croogo_bootstrap.php
+++ Config/croogo_bootstrap.php
@@ -87,14 +87,14 @@
 	}
 	foreach ($plugins as $plugin) {
 		$pluginName = Inflector::camelize($plugin);
-		if (!file_exists(APP . 'Plugin' . DS . $pluginName)) {
+		if (!is_dir(APP . 'Plugin' . DS . $pluginName)) {
 			CakeLog::write(LOG_ERR, 'Plugin not found during bootstrap: ' . $pluginName);
 			continue;
 		}
 		$bootstrapFile = APP . 'Plugin' . DS . $pluginName . DS . 'Config' . DS . 'bootstrap.php';
-		$bootstrap = file_exists($bootstrapFile);
+		$bootstrap = is_file($bootstrapFile);
 		$routesFile = APP . 'Plugin' . DS . $pluginName . DS . 'Config' . DS . 'routes.php';
-		$routes = file_exists($routesFile);
+		$routes = is_file($routesFile);
 		$option = array(
 			$pluginName => array(
 				'bootstrap' => $bootstrap,
